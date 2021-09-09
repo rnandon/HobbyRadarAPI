3977b2c5ac317c5b364658b30ee648ddf580e488
Ryan <ryan@ryannd.com>
2021-09-09 15:29:32 -0500

    Init

---
+++ HobbyRadarAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HobbyRadarAPI.Configuration;
+using HobbyRadarAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HobbyRadarAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RolesConfiguration());
+        }
+
+    }
+}
---
+++ HobbyRadarAPI/Managers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HobbyRadarAPI.DataTransferObjects;
+using HobbyRadarAPI.Models;
+
+namespace HobbyRadarAPI.Managers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserForRegistrationDto, User>();
+        }
+    }
+}
